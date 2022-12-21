7806d4a508c1d033c421f3fb9fa002a30d42a562
Lilith Avdalyan <52968837+sociofriend@users.noreply.github.com>
2022-12-21 05:28:06 -0800

    Add files via upload

---
+++ Polygon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Homework_1
+{
+	internal class Polygon
+	{
+		public int AreaFinder(int n)
+		{
+
+			int middleRowArea = 2 * n - 1;
+			int totalArea = middleRowArea;
+
+			if (n == 0)
+				{
+					return n;
+				}
+			else if (n < 0)
+			{
+				Console.WriteLine("a positive sign intereger is required");
+			}
+			else
+			{
+				while (2 * n - 1 != 0)
+				{
+
+					n--;
+					if (n != 0)
+					{
+						int nextRowsArea = 2 * (2 * n - 1);
+						totalArea += nextRowsArea;
+					}
+
+					else
+						return totalArea;
+				}
+            }
+
+            return totalArea;
+
+
+        }
+		
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Homework_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Polygon obj = new Polygon();
+            int res = obj.AreaFinder(3);
+            Console.WriteLine(res);
+        }
+    }
+}
+
